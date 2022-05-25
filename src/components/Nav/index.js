20df5cd571d5a44491562a2c0e176e9681a26903
bryce <blarkin13@gmail.com>
2022-05-25 02:12:33 -0500

    nav and about sections

---
+++ src/components/Nav/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function Nav() {
+
+const categories = [
+    {
+        name: "wedding",
+        description:
+        "Photos of customer order wedding cakes",
+    },
+    { name: "birthday",
+      description: 
+      "Photos of customer order birthday cakes",
+    },
+];    
+
+function categorySelected(name) {
+    console.log(`${name} clicked`)
+}
+
+    return(
+        <header>
+            <nav className='nav'>
+                <ul className='flex-row'>
+                    <li className='mx-2'>
+                        <a href="/" className="site-title">
+                            FEC
+                        </a>
+                    </li>
+                    {categories.map((category) => (
+                        <li
+                        className='mx-1'
+                        key={category.name}
+                        >
+                            <span onClick={() => categorySelected(category.name)}>
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+
+}
+
+export default Nav;
